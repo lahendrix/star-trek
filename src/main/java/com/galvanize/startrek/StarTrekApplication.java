3eae0c21f81c4ff395a8a34c29269faf07107c2d
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-03-18 13:03:33 -0500

    Initial commit.

---
+++ src/main/java/com/galvanize/startrek/StarTrekApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.startrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarTrekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarTrekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galvanize/startrek/StarTrekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.startrek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarTrekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
