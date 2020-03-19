46550e53036f563856b23c7ec1c2a1ced1098ddd
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-03-19 13:01:05 -0500

    Step 6: Add a constructor to OfficerService that accepts OfficerRepository

--- src/main/java/com/galvanize/startrek/OfficerService.java
+++ src/main/java/com/galvanize/startrek/OfficerService.java
@@ -5,4 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 class OfficerService {
 
+    OfficerRepository officerRepository;
+
+    public OfficerService(OfficerRepository repository) {
+        this.officerRepository = repository;
+    }
 }
