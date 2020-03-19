520a672298bfa76d7c1dda53b5e0a0996e900760
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-03-19 12:30:00 -0500

    Step 1: Create a test file for your controller with necessary MockMvc imports. Static imports are preferred here.

--- src/test/java/com/galvanize/startrek/OfficerControllerTest.java
+++ src/test/java/com/galvanize/startrek/OfficerControllerTest.java
@@ -1,2 +1,18 @@
 package com.galvanize.startrek;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OfficerControllerTest {
+
+}
