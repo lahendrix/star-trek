d1abc44e551bce507f4175257a6c5ba7da83ebe0
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-03-19 12:35:36 -0500

    Step 2: Write failing controller test for the zero case

--- src/test/java/com/galvanize/startrek/OfficerControllerTest.java
+++ src/test/java/com/galvanize/startrek/OfficerControllerTest.java
@@ -1,10 +1,15 @@
 package com.galvanize.startrek;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -15,4 +20,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class OfficerControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    OfficerService officerService;
+
+    @Test // Zero case from ZOMBIES
+    void getAllOfficers_whenNoOfficersExist_returnsEmpty() throws Exception{
+        // Setup
+        int expected = 0;
+        when(officerService.getAllOfficers()).thenReturn(new ArrayList<>());
+
+        // Exercise & Assert
+        mockMvc.perform(get("/api/officers"))
+                .andDo((print()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(expected)));
+    }
 }
