4d9716e870435334265f27627182fb6a299f10ea
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-03-19 12:40:06 -0500

    Step 3: Create an OfficerServiceTest file

---
+++ src/test/java/com/galvanize/startrek/OfficerServiceTest.java
@@ -0,0 +1,22 @@
+package com.galvanize.startrek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OfficerServiceTest {
+}
