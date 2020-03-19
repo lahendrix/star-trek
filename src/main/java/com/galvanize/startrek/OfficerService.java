a55d1151f9821c56be71e68dfbb1479e91252a1e
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-03-19 12:49:28 -0500

    Step 4: Create a failing test for OfficerService.getAllOfficers and a new OfficerService class

---
+++ src/main/java/com/galvanize/startrek/OfficerService.java
@@ -0,0 +1,8 @@
+package com.galvanize.startrek;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class OfficerService {
+
+}
--- src/test/java/com/galvanize/startrek/OfficerServiceTest.java
+++ src/test/java/com/galvanize/startrek/OfficerServiceTest.java
@@ -1,10 +1,11 @@
 package com.galvanize.startrek;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -19,4 +20,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class OfficerServiceTest {
+    @Mock
+    OfficerRepository officerRepository;
+
+    @Test
+    void getAllOfficers_whenNoOfficersExist_returnsEmpty() {
+        // Setup
+        OfficerService officerService = new OfficerService(officerRepository);
+        int expected = 0;
+
+        // Exercise
+        int actual = officerService.getAllOfficers();
+
+        // Assert
+        assertEquals(expected, actual);
+
+    }
 }
